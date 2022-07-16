70ff8423d5378d1076bbdd9fec665c891420026e
ikiwz-XD <93254944+ikiwz-XD@users.noreply.github.com>
2022-07-16 15:01:19 +0700

    Update and rename run.py to zoo.py

--- zoo.py
+++ zoo.py
@@ -215,7 +215,7 @@ def menu(my_name,my_id):
 	print('')
 	print(f'    {p}     —>[•] 1. Crack Publik       [ fitur √ ] ')
 	print('         —>[•] 2. Crack Pengikut     [ fitur √ ] ')
-	print(f'         —>[•] 3. Crack Grup {m}        [ Fitur x ] ')
+	print(f'         —>[•] 3. Ganti User Agent ')
 	print(f'{p}         —>[•] 4. Crack File         [ Fitur √ ] ')
 	print('         —>[•] 5. Hasil Crack  ')
 	print(f'         —>[•] 0. Keluar       {x}')
@@ -226,7 +226,7 @@ def menu(my_name,my_id):
 	elif _____alvino__adijaya_____ in ['2']:
 		dump_pengikut()
 	elif _____alvino__adijaya_____ in ['3']:
-		grup()
+		uas()
 	elif _____alvino__adijaya_____ in ['4']:
 		crack_file()
 	elif _____alvino__adijaya_____ in ['5']:
@@ -1094,6 +1094,44 @@ def cek_apk(session,cookie):
 	else:
 		for i in range(len(game)):
 			print("   %s%s. %s%s"%(K,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+
+# GANTI USER AGENT
+def useragent():
+	print ("\n%s [%s01%s] Ganti user agent "%(P,K,P))
+	print (" [%s02%s] Cek user agent "%(K,P))
+	print (" [%s00%s] Kembali "%(M,P))
+	uas()
+def uas():
+    u = raw_input('\n%s [?] pilih :%s '%(P,K))
+    if u == '':
+        print("%s [!] Isi yang benar kentod "%(M));jeda(2);uas()
+    elif u in("1","01"):
+    	print (" %s[%s*%s] ketik %sMy user agent%s di browser google chrome\n [%s*%s] untuk gunakan user agent anda sendiri"%(P,K,P,H,P,K,P))
+    	print (" [%s*%s] ketik %sdefault%s untuk gunakan user agent bawaan tools"%(K,P,H,P))
+    	try:
+    	    ua = raw_input("%s [?] user agent : %s"%(P,K))
+            if ua in(""):
+            	print("%s [!] Isi yang benar kentod "%(M));jeda(2);menu()
+            elif ua in("my user agent","My User Agent","MY USER AGENT","My user agent"):
+            	jalan("%s [!]  Anda akan di arahkan ke browser "%(H));jeda(2)
+            	os.system("am start https://www.google.com/search?q=My+user+agent>/dev/null");jeda(2);useragent()
+            elif ua in("default","Default","DEFAULT"):
+                ua = 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]'
+                open("data/ua.txt","w").write(ua_)
+                print ("\n%s [√] menggunakan user agent bawaan"%(H));jeda(2);menu()
+            open("data/ua.txt","w").write(ua);jeda(2)
+            print ("\n%s [√] berhasil mengganti user agent"%(H));jeda(2);menu()
+        except KeyboardInterrupt as er:
+			exit ("\x1b[1;91m [!] "+er) 
+    elif u in("2","02"):
+        try:
+        	ua_ = open('data/ua.txt', 'r').read();jeda(2);print ("%s [%s*%s] user agent anda : %s%s"%(P,K,P,H,ua_));jeda(2);raw_input("\n%s [ %senter%s ] "%(P,K,P));menu()
+        except IOError:
+        	ua_ = '%s-'%(M)
+    elif u in("0","00"):
+    	menu()
+    else:
+        print("%s [!] Isi yang benar kentod "%(M));jeda(1);uas()
 
 #-----------------------[ SYSTEM-CONTROL ]--------------------#
 if __name__=='__main__':
